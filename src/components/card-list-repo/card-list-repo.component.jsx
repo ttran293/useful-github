65ffc3377e871461d91d2b59192e033037ecbb82
Thanh Nam Tran <ttran19@umbc.edu>
2022-04-08 00:47:55 -0400

    set up fetch for repo list

--- src/components/card-list-repo/card-list-repo.component.jsx
+++ src/components/card-list-repo/card-list-repo.component.jsx
@@ -4,14 +4,40 @@ import githubRepos from "../../data/githubRepos";
 import SearchBox from "../search-box/search-box.component";
 import { useState, useEffect } from "react";
 
+
+const List = [
+  { name: "framer/motion" },
+  { name: "jwasham/coding-interview-university" },
+  { name: "animate-css/animate.css" },
+  { name: "mui/material-ui" },
+  { name: "khang-nd/7.css" },
+  { name: "public-apis/public-apis" },
+  { name: "sindresorhus/awesome" },
+  { name: "danistefanovic/build-your-own-x" },
+  { name: "sdmg15/Best-websites-a-programmer-should-visit" },
+  { name: "EbookFoundation/free-programming-books" },
+  { name: "practical-tutorials/project-based-learning" },
+  { name: "vinta/awesome-python" },
+  { name: "tensorflow/tensorflow" },
+  { name: "bradtraversy/design-resources-for-developers" },
+  { name: "florinpop17/app-ideas" },
+  { name: "matheusfelipeog/beautiful-docs" },
+  { name: "thedaviddias/Front-End-Checklist" },
+];
+
 function CardListRepo() {
   const [repos, setRepos] = useState([]);
   const [filteredRepos, setFilterRepos] = useState(repos);
   const [searchField, setSearchField] = useState("");
 
-  useEffect(() => {
-    setRepos(githubRepos);
-  }, []);
+   useEffect(() => {
+     Promise.all(
+       List.map(async (e) => {
+         const res = await fetch("https://api.github.com/repos/" + e.name);
+         return await res.json();
+       })
+     ).then((results) => setRepos(results));
+   }, []);
 
   useEffect(() => {
     const newFilteredRepos = repos.filter((repo) => {
@@ -38,7 +64,7 @@ function CardListRepo() {
         onChangeHandler={onSearchChange}
       />
       <div className="card-list">
-        {filteredRepos.map((repo) => {
+        {filteredRepos.length && filteredRepos.map((repo) => {
           return <CardRepo key={repo.id} repo={repo} />;
         })}
       </div>
